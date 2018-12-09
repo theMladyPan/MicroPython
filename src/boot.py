fe068c79b2884bc1e41024c487bdfcc779e91019
theMladyPan <stanislav@rubint.sk>
2018-12-09 22:12:30 +0100

    reset servo at boot

--- src/boot.py
+++ src/boot.py
@@ -3,11 +3,18 @@
 #esp.osdebug(None)
 import gc
 import webrepl
+import machine
+import settings
+import network
 webrepl.start()
 gc.collect()
 
+def reset_servo():
+    pins=[5,4,0,2,14,12,13,15]
+    for pin in pins:
+        machine.PWM(machine.Pin(pin),freq=settings.pwm_freq, duty=settings.start_duty)
+
 def do_connect():
-    import network
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
@@ -16,5 +23,6 @@ def do_connect():
         while not sta_if.isconnected():
             pass
     print('network config:', sta_if.ifconfig())
-    
+
+reset_servo()
 do_connect()
--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ def main(): #main routine
     pins=[5,4,0,2,14,12,13,15]
     lp, lz, pp, pz = LeftFront(0,2), LeftRear(14,12), RightFront(5,4), RightRear(13,15)
     legs = [lp, lz, pp, pz]
-    pause = 0.04 #int(input("Pause [ms]"))/1000.0
+    pause = 0.035 #int(input("Pause [ms]"))/1000.0
     while 1:
         for i in range(0,500,20):
             for leg in range(len(legs)):
