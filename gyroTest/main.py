7ce3cc05fb647ecd385cc8c8efdb9b06b49f5955
theMladyPan <stanislav@rubint.sk>
2020-07-30 12:13:44 +0200

    working measurement of wave

--- gyroTest/main.py
+++ gyroTest/main.py
@@ -1,13 +1,25 @@
 import time
 import mpu6050
 import micropython
-from machine import Pin
-micropython.alloc_emergency_exception_buf(100)
+from machine import Pin, freq
+import time
 
-def isr(pin):
-    print("Interrupt!")
+# micropython.alloc_emergency_exception_buf(100)
 
-mpu = mpu6050.Mpu6050(5,4)
-mpu.get_values()
-#server = mpuserver.MPUServer(mpu)
-#server.serve()
+freq(int(16e7)) #160MHz
+mpu = mpu6050.Mpu6050(scl=5, sda=4) #D1=SCL, D2=SDA
+
+input("\n########\nStart ?")
+
+while True:
+    n = int(input("nOfSamples: "))
+    with open("data.csv", "w") as subor:
+        v=[]
+        t=[]
+        print("Starting measurement ...")
+        for i in range(n):
+            v.append(mpu.get_acz())
+            t.append(time.ticks_ms())
+        print("Measuring ended!\nWriting to memory ...")
+        for i in range(n):
+            zapisane = subor.write("\n"+str(t[i])+";"+str(v[i]))
