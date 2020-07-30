bfd51c0cf64a4c05fe5222522ae66b307104ee6d
theMladyPan <stanislav@rubint.sk>
2020-07-30 12:14:39 +0200

    measurement of distance working example

--- gyroTest/main.py
+++ gyroTest/main.py
@@ -1,13 +1,37 @@
-import time
-import mpu6050
-import micropython
+# HC-SR04 Ultrasound Sensor
+import time, machine
 from machine import Pin
-micropython.alloc_emergency_exception_buf(100)
 
-def isr(pin):
-    print("Interrupt!")
 
-mpu = mpu6050.Mpu6050(5,4)
-mpu.get_values()
-#server = mpuserver.MPUServer(mpu)
-#server.serve()
+# WeMos D4 maps GPIO2 machine.Pin(2) = TRIGGER
+# WeMos D2 maps GPIO4 machine.Pin(4) = ECHO
+
+machine.freq(int(16e7)) 
+triggerPort = 5
+echoPort = 4
+
+ptrig = Pin(triggerPort, Pin.OUT)
+pecho = Pin(echoPort, Pin.IN)
+
+input("Start?")
+
+def trigger(us=10):
+    ptrig.on()
+    time.sleep_us(us)
+    ptrig.off()
+
+def measure():                                                                                                                                    
+    trigger()                                                                                                                                  
+    while not pecho.value(): pass     
+    start = time.ticks_us()                                                                                                                       
+    while pecho.value(): pass                                                                                                        
+    end = time.ticks_us()   
+    return time.ticks_diff(end, start)
+
+while True:
+    # speed of sound = 340.29m/s
+    vs = 340.29
+    raw_dist = measure()
+    print("Distance: " + str(raw_dist*vs/2000) + "mm", end="\r")
+    time.sleep(0.1)
+                                                                                                                                                
