e2a396f199a60e02dcc8973e16dd716eaaafdcda
theMladyPan <stanislav@rubint.sk>
2021-12-28 18:01:27 +0100

    10mA quiescent led light controller

--- src/LED_Svetlo/main.py
+++ src/LED_Svetlo/main.py
@@ -1,77 +1,25 @@
-from machine import Pin, PWM, freq, TouchPad
-from time import sleep
-import socket
-import network
-import _thread
+from machine import Pin, PWM, TouchPad, lightsleep, freq
+import time 
 
-LOW = "128"
-HIGH = "512"
+freq(20000000)
 
 tp33 = TouchPad(Pin(33))
-ap = network.WLAN(network.AP_IF) # create access-point interface
-pwm0 = PWM(Pin(2), freq=1000, duty=0) # create and configure in one go
+tp12 = TouchPad(Pin(12))
+pwm0 = PWM(Pin(2), freq=10000, duty=0) # create and configure in one go
+duty = 1
 
-ldf = open("duty.txt", "r")
-last_duty = ldf.read()
-ldf.close()
-ldf = open("duty.txt", "w")
-if last_duty == LOW:
-    ldf.write(HIGH)
-else:
-    ldf.write(LOW)
-ldf.close()
-pwm0.duty(int(last_duty))
-
-def loop():
-    tpr = tp33.read()
-    if tpr<200:
-        last_s = ap.active()
-        ap.active(not last_s)
-        print("Wifi toggle")
-    sleep(0.2)
-
-t_parallel = _thread.start_new_thread(loop, ())
-
-ap.config(essid='RA') # set the ESSID of the access point
-ap.config(max_clients=10) # set how many clients can connect to the network
-ap.active(True)         # activate the interface
-
-def blink(pwm, n=4, t=0.1):
-    duty = pwm.duty()
-    for i in range(1,n+1):
-        pwm0.duty(1)
-        sleep(t)
-        pwm0.duty(0)
-        sleep(t)
-    pwm.duty(duty)
-
-blink(pwm0, 3)
-
-addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
-s = socket.socket()
-s.bind(addr)
-s.listen(1)
-
-print('listening on', addr)
-
-while True:
-    cl, addr = s.accept()
-    # link(pwm0, 2)
-    rcvd = cl.read()
-    print('client connected from', addr, "received data:", rcvd)
-
-    cl.close()
-    try:
-        duty = max(0, int(rcvd.rstrip(b"\n").decode()))
-        pwm0.duty(duty)
-        print("Duty set to %d"%duty)
-    except ValueError:
-        try:
-            cmd = rcvd.rstrip(b"\n").decode()
-            if "off" in cmd.lower():
-                ap.active(False)
-        except:
-            pass
-    except:
-        pass
+while 1:
+    t33v = tp33.read()
+    t12v = tp12.read()
+    # print(duty)
+    if t12v < 256:  # touched p12
+        duty = int(duty/1.1)
+        if duty < 1: 
+            duty = 1
+    elif t33v < 256:
+        duty += 1
+        duty = int(duty*1.1)
+        if duty > 1023:
+            duty = 1023
+    pwm0.duty(duty)
+    time.sleep_ms(50)
