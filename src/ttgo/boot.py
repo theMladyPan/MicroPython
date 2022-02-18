dbad92e54435ef0ea9202e1a8ef8c0a9e4554737
theMladyPan <stanislav@rubint.sk>
2022-02-18 22:36:09 +0100

    functional netscan & value reader

--- src/ttgo/boot.py
+++ src/ttgo/boot.py
@@ -5,13 +5,14 @@
 
 #webrepl.start()
 
+
 import network, machine, ntptime, st7789, gc, esp, time
 from st7789 import color565, sysfont
 
 from machine import RTC, ADC, Pin, SPI, UART, Timer
 from config import ssid, password, my_addr, TX_EN_Pin
 from utils import wifi_list
-from xerxes import send_msg
+from xerxes import send_msg, read_msg, read_pleaf_data, read_raw
 
 def mhz(n):
   return n*10**6
@@ -33,16 +34,40 @@ Button2_Pin = 0;  #left button
 HORIZONTAL = 5
 VERTICAL=0
 
+
 MODE=HORIZONTAL
 
 def reset(p):
   display.fill(0)  
 
+class Flag:
+    def __init__(self, value=False):
+        self.value = value
+    
+    def set(self):
+        self.value = True
+        
+    def unset(self):
+        self.value = False
+        
+    def toggle(self):
+        self.value = not self.value
+        
+    def is_set(self):
+        return self.value
 
 def message(text="", x=0, y=0, size=1, clear=False):
   if clear:
     display.fill(0)
   display.text((x, y), text, st7789.WHITE, sysfont, size, nowrap=False)
+  
+def multiline(text="", x=0, y=0, size=1, clear=False):
+  if clear:
+    display.fill(0)
+  v_offset = y
+  for i in text.split("\n"):
+    message(i, x=x, y=v_offset, size=size, clear=False)
+    v_offset += size*9
     
 
 def do_connect(block=False):
@@ -85,9 +110,53 @@ def calibrate(p):
         min[i] = last[i]
       elif last[i] > max[i]:
         max[i] = last[i]
+        
+uart1=None
 
-
+def netscan():
+  leaves = []
+  for i in range(1, 255):
+    send_msg(uart1, my_addr, i.to_bytes(1, "big"), b"")  # write empty payload
+    time.sleep(0.005)
+    if uart1.any():
+        reply = read_raw(uart1, timeout=0.005)       
+        leaves.append(reply["source"])
+  print("Found: ")
+  print(", ".join(leaves))
+  message(", ".join(leaves), size=2, clear=True)
   
+def read_all_leaves():         
+    for i in range(1, 255):
+        send_msg(uart1, my_addr, i.to_bytes(1, "big"), b"")  # write empty payload
+        time.sleep(0.005)
+        if uart1.any():
+            try:
+                reply = read_msg(uart1, timeout=0.005)    
+            except RuntimeError:
+                # read out rest of broken message
+                uart1.read()
+                continue
+            
+            print(reply)
+            readings = read_pleaf_data(reply["payload"])
+            
+            multiline(
+                reply["source"] + 
+                "\np: %.2f mm" % readings["pressure"] +
+                "\nts: %.2f oC" % readings["temp_sens"] + 
+                "\nte1: %.2f oC" % readings["temp_ext1"] + 
+                "\nte2: %.2f oC" % readings ["temp_ext2"],
+                clear=True,
+                size=3
+            )
+        
+def read_all_int(pin=None):
+    print("starting reading...")
+    read_all_flag.set()
+
+def netscan_int(pin=None):
+    netscan_flag.set()       
+    
 print("Mem free:", gc.mem_free())
 
 button1 = Pin(Button1_Pin, Pin.IN, Pin.PULL_UP)
@@ -96,7 +165,7 @@ tx_en = Pin(TX_EN_Pin, Pin.OUT, value=0)
 
 BLK = Pin(BL_Pin, Pin.OUT)
 spi = SPI(baudrate=40000000, miso=Pin(MISO_Pin), mosi=Pin(MOSI_Pin, Pin.OUT), sck=Pin(SCLK_Pin, Pin.OUT))
-display = st7789.ST7789(spi, 240, 135, cs=Pin(CS_Pin), dc=Pin(DC_Pin), rst=None, xstart=40, ystart=53   ) # 135/240
+display = st7789.ST7789(spi, 240, 135, cs=Pin(CS_Pin), dc=Pin(DC_Pin), rst=None, xstart=40, ystart=53   ) # 135/240
 
 # init display
 display._set_mem_access_mode(MODE, False, False, False)
@@ -104,30 +173,29 @@ display.fill(0) #filling the display with black
 BLK.value(1) #turn backlight on
 display._set_mem_access_mode(MODE, False, False, True) #setting screen orientation (rotation (0-7), vertical mirror, horizonatal mirror, is bgr)
 
-
 # bind button interrupts
-button1.irq(trigger=Pin.IRQ_FALLING, handler=wifi_list) #interrupt for right button (button 2)
-button2.irq(trigger=Pin.IRQ_FALLING, handler=reset) #interrupt for right button (button 2)
+button1.irq(trigger=Pin.IRQ_FALLING, handler=read_all_int) #interrupt for right button (button 1)
+button2.irq(trigger=Pin.IRQ_FALLING, handler=netscan_int) #interrupt for left button (button 2)
 
+read_all_flag = Flag()
+netscan_flag = Flag()
+uart1 = UART(1, baudrate=115200, tx=22, rx=21, timeout=5, timeout_char=5)
 
-uart1 = UART(1, baudrate=115200, tx=22, rx=21, timeout=10, timeout_char=5)
-
-
-tim1 = Timer(1)
+# tim1 = Timer(1)
 #tim1.init(period=1000, mode=Timer.PERIODIC, callback=request)
 
-#clock(display, 0)
+message("P sensors   ->", size=3)
+message("Netscan     ->", size=3, y=100)
 
-while(1):
-  for i in range(255):
-    send_msg(uart1, my_addr, i.to_bytes(1, "big"), b"")  # write empty payload
-    time.sleep(0.01)
-  while(uart1.any() == 0):
-    pass #  wait until something is received
-  
-  m = uart1.read()         # read up to 5 bytes
-  message(str(m), size=1, clear=True)
-  time.sleep(1)
+while 1: 
+    if read_all_flag.is_set():
+        read_all_leaves()
+    elif netscan_flag.is_set():
+        netscan()
+    time.sleep(0.1)
+  # m = uart1.read()         # read up to 5 bytes
+  # message(str(m), size=1, clear=True)
+  # time.sleep(2)
 
 #message("Connecting to: "+ssid)
 #do_connect()
--- src/ttgo/xerxes.py
+++ src/ttgo/xerxes.py
@@ -1,5 +1,5 @@
 import struct
-
+import time
 
 def checksum(message: bytes) -> bytes:
     summary = sum(message)
@@ -22,7 +22,7 @@ def send_msg(com, sender, destination, payload: bytes, *, tx_en=None):
     msg += checksum(msg)
 
     if tx_en:
-        # activate transmission mode
+        # activate transmission mode
         tx_en.on()
 
     com.write(msg)
@@ -32,33 +32,118 @@ def send_msg(com, sender, destination, payload: bytes, *, tx_en=None):
         tx_en.off()
     #time.sleep(0.001)
     #com.write(os.urandom(128))
+    
+class StopWatch:
+    def __init__(self, timeout=0):
+        self.start = time.time()
+        self.timeout = timeout
+        
+    def elapsed(self):
+        if self.timeout:
+            return ( time.time() - self.start ) > self.timeout
+        else:
+            return False
+    
+    def running(self):
+        if self.timeout:
+            return ( time.time() - self.start ) < self.timeout
+        else:
+            return True
+        
+def to_hex(byte):
+    return hex(struct.unpack("!B", byte)[0])
 
-def read_msg(com):
+def read_raw(com, *, timeout=0):
     # wait for start of message
+    sw = StopWatch(timeout)
+        
     next_byte = com.read(1)
     while next_byte != b"\x01":
         next_byte = com.read(1)
-        if len(next_byte)==0:
-            raise TimeoutError("No message in queue")
+        if sw.elapsed():
+            return None
 
     checksum = 0x01
     # read message length
-    msg_len = int(com.read(1).hex(), 16)
+    # msg_len = int(com.read(1).hex(), 16)
+    msg_len = struct.unpack("!B", com.read(1))[0]
+    print(msg_len)
+    
     checksum += msg_len
 
+    if sw.elapsed(): return None
     #read source and destination address
     src = com.read(1)
+    if sw.elapsed() or src == None: return None
     dst = com.read(1)
+    if sw.elapsed() or dst == None: return None
 
     sot = com.read(1)
+    if sw.elapsed(): return None
 
     for i in [src, dst, sot]:
-        checksum += int(i.hex(), 16) 
+        checksum += struct.unpack("!B", i)[0]
+    
+    # read and unpack all data into array, assiming it is uint32_t, big-endian
+    msg_array = []
+    for i in range(int(msg_len-6)):
+        next_byte = com.read(1)
+        uchar = struct.unpack("!B", next_byte)[0]
+        checksum += uchar
+        msg_array.append(next_byte)
+    
+    #read checksum
+    rcvd_chks = com.read(1)
+    checksum += struct.unpack("!B", rcvd_chks)[0]
+    checksum %= 0x100
+    if checksum:
+        print("received checksum: ", checksum)
+        raise IOError("Invalid checksum received")
+
+    return {
+        "source": to_hex(src),
+        "destination": to_hex(dst),
+        "length": msg_len,
+        "payload": msg_array 
+        }
+
+def read_msg(com, *, timeout=0):
+    # wait for start of message
+    sw = StopWatch(timeout)
+        
+    next_byte = com.read(1)
+    while next_byte != b"\x01":
+        next_byte = com.read(1)
+        if sw.elapsed():
+            return None
+
+    checksum = 0x01
+    # read message length
+    # msg_len = int(com.read(1).hex(), 16)
+    msg_len = struct.unpack("!B", com.read(1))[0]
+    print(msg_len)
+    
+    checksum += msg_len
+
+    if sw.elapsed(): return None
+    #read source and destination address
+    src = com.read(1)
+    if sw.elapsed() or src == None: return None
+    dst = com.read(1)
+    if sw.elapsed() or dst == None: return None
+
+    sot = com.read(1)
+    if sw.elapsed(): return None
+
+    for i in [src, dst, sot]:
+        checksum += struct.unpack("!B", i)[0]
 
     # read message ID
     msg_id_raw = com.read(4)
+    if sw.elapsed(): return None
+    
     if(len(msg_id_raw)!=4):
-        raise IOError("Invalid message received")
+        raise RuntimeError("Invalid message received")
     for i in msg_id_raw:
         checksum += i
 
@@ -74,15 +159,30 @@ def read_msg(com):
     
     #read checksum
     rcvd_chks = com.read(1)
-    checksum += int(rcvd_chks.hex(), 16)
+    checksum += struct.unpack("!B", rcvd_chks)[0]
     checksum %= 0x100
     if checksum:
+        print("received checksum: ", checksum)
         raise IOError("Invalid checksum received")
 
     return {
-        "source": src,
-        "destination": dst,
+        "source": to_hex(src),
+        "destination": to_hex(dst),
         "length": msg_len,
         "message_id": msg_id,
         "payload": msg_array 
         }
+
+def to_celsius(millikelvin):
+    return (millikelvin / 1000) - 273.15
+
+def to_mmH2O(microbar):
+    return ( microbar / 1000000 ) * 10197.162129779;
+
+def read_pleaf_data(payload):
+    return {
+        "pressure": to_mmH2O(payload[0]),
+        "temp_sens": to_celsius(payload[1]),
+        "temp_ext1": to_celsius(payload[2]),
+        "temp_ext2": to_celsius(payload[3])
+    }
