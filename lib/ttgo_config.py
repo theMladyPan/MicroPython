2cfe6935293dc60468787bf6267380054bf229a7
theMladyPan <stanislav@rubint.sk>
2022-05-26 23:45:19 +0200

    ttgo config update

--- lib/ttgo_config.py
+++ lib/ttgo_config.py
@@ -11,8 +11,13 @@ class TTGO_Pins:
     DC = 16    # data/command pin
     CS = 5     # chip select pin
 
-    Button1 = 35; # right button
-    Button2 = 0;  # left button
+    Button1 = 35 # right button
+    Button2 = 0  # left button
+
+    Lbtn = Button2
+    Rbtn = Button1
+
+    BAT = 14
 
 HORIZONTAL = 5
 VERTICAL=0
@@ -23,3 +28,49 @@ ORIENTATIONS = [
     (0xc0, 135, 240, 53, 40), 
     (0xa0, 240, 135, 40, 52)
 ]
+
+display = None
+font_small, font_big = None, None
+orientation = None
+
+
+def bind_display(disp, small, big, orient):
+    global display, font_small, font_big, orientation
+    display = disp
+    font_small = small
+    font_big = big
+    orientation = orient
+
+
+def message(text="", x=0, y=0, big=True, clear=False, wrap=True):
+    global display, font_big, font_small, orientation
+    if display is None:
+        raise Exception("Open display and bind it first")
+
+    if clear:
+        display.fill(0)
+    if orientation == 0 or orientation == 2:
+        if big:
+            font = font_big
+            cl = 8
+            yo = 30
+        else:
+            font = font_small  
+            cl = 16
+            yo = 15
+    else:
+        if big:
+            font = font_big
+            cl = 15
+            yo = 30
+        else:
+            font = font_small  
+            cl = 30
+            yo = 15
+    if len(text) > cl and wrap:
+        for i in range(len(text)/cl):
+            display.text(font, text[i*cl : i * cl + 15], x, y)
+            y += yo
+            
+    else:
+        display.text(font, text, x, y)
